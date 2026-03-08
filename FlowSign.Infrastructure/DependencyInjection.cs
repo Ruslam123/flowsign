c7b2b8147258674f1670d22bea97f9a200e57191
Ruslam123 <ruslanyacenko2003@gmail.com>
2026-03-08 20:36:36 +0200

    35

--- FlowSign.Infrastructure/DependencyInjection.cs
+++ FlowSign.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,18 @@
-﻿namespace FlowSign.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FlowSign.Application.Interfaces.Repositories;
+using FlowSign.Infrastructure.Repositories;
+using FlowSign.Infrastructure.Persistence;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-	{
-		services.AddScoped<ITemplateService, TemplateService>();
-		services.AddScoped<IEmailService, EmailService>();
-		return services;
+    {
+        services.AddDbContext<FlowSignDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("Default")));
+        services.AddScoped<IDocumentRepository, DocumentRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IAuditLogRepository, AuditLogRepository>();
+        return services;
     }
-}
-
 }
