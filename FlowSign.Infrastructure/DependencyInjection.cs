ee2c73649bf65fa92d3e9866afd703f2dc2f9a2f
Ruslam123 <ruslanyacenko2003@gmail.com>
2026-03-08 20:27:23 +0200

    34

---
+++ FlowSign.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowSign.Infrastructure
+
+public class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddScoped<ITemplateService, TemplateService>();
+		services.AddScoped<IEmailService, EmailService>();
+		return services;
+    }
+
+}
